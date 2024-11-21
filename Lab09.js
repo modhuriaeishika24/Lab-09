50bdd18811e40ffe1b3f9ef4a716cb014441c374
modhuriaeishika24 <mbe7@njit.edu>
2024-11-21 11:14:11 -0500

    save progess

---
+++ Lab09.js
@@ -0,0 +1,28 @@
+//q 1
+
+
+//q 2
+let firstName = "Harry";
+let house = "Gryffindor";
+console.log(`Welcome, ${firstName} of ${house}!`);
+//q 3
+let characterName = "Harmione";{
+console.log(characterName.toUpperCase());
+console.log(characterName.toLowerCase());
+}
+//q 4
+let spell = "Expelliarmus";
+console.log(spell.trim());
+//q 5
+let quote = "I solemnly swear that I am up to no good.";
+console.log(quote.slice(11, 16));
+//q 6
+let firstname = "Ron";
+let lastName = " Weasley";
+console.log(firstname.concat(lastName));
+//q 7
+let sentance = "Draco is a good wizard";
+console.log(sentance.replace("good", "bad"));
+//q 8
+let houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw',],"Slytherin";
+console.log(houses.push(4));
